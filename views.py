e4d559ba5c4f55f36dd3f1f553fd64dd4a4c8edf
김세희 <sadie1001@naver.com>
2022-10-25 23:08:16 +0900

    feat : 테이블 css 구현 완료

--- views.py
+++ views.py
@@ -1,25 +1,41 @@
 from flask import Flask, render_template, request
-from collections import deque
 app = Flask(__name__)
 
 
 ## HTML을 주는 부분
 @app.route('/')
 def home():
-   todayReservations = deque([ { 'type' : 'laundry', 'room' : '325', 'username' :
-'김철수', 'time' : 7, 'day' : 'today' }, { 'type' : 'dry', 'room' : '326', 'username'
-: '홍길동', 'time' : 10, 'day' : 'today' }, { 'type' : 'laundry', 'room' : '325',
-'username' : '홍길동', 'time' : 11, 'day' : 'today' }, { 'type' : 'laundry', 'room' :
-'325', 'username' : '김철수', 'time' : 11, 'day' : 'today' }, { 'type' : 'dry', 'room'
-: '325', 'username' : '김철수', 'time' : 11, 'day' : 'today' }, { 'type' :
-'laundry', 'room' : '325', 'username' : '김철수', 'time' : 12, 'day' : 'today' }, {
-'type' : 'laundry', 'room' : '325', 'username' : '김철수', 'time' : 14, 'day' :
-'today' }, { 'type' : 'dry', 'room' : '325', 'username' : '김철수', 'time' : 18, 'day'
-: 'today' }, { 'type' : 'laundry', 'room' : '325', 'username' : '김철수', 'time' :
-20, 'day' : 'today' }, { 'type' : 'dry', 'room' : '325', 'username' : '김철수', 'time'
-: 22, 'day' : 'today' }, ])
 
-   return render_template('table.html', todayReservations=todayReservations)
+
+
+   todayReservations = [
+      { 'type' : 'laundry', 'room' : '325', 'time' : 7 },
+      { 'type' : 'dry', 'room' : '326',  'time' : 10,  },
+      { 'type' : 'laundry', 'room' : '325', 'time' : 11 },
+      { 'type' : 'laundry', 'room' : '325','time' : 11 },
+      { 'type' : 'dry', 'room': '325',  'time' : 11 },
+      { 'type' : 'laundry', 'room' : '325',  'time' : 12 },
+      { 'type' : 'laundry', 'room' : '325',  'time' : 14 },
+      { 'type' : 'dry', 'room' : '325',  'time' : 18 },
+      { 'type' : 'laundry', 'room' : '325',  'time' :20 },
+      { 'type' : 'dry', 'room' : '325',  'time': 22 }
+   ]
+
+   tomorrowReservations = [
+   { 'type' : 'laundry', 'room' : '325', 'time' : 7 },
+    { 'type' : 'dry', 'room' : '326',  'time' : 7,  },
+    { 'type' : 'laundry', 'room' : '325', 'time' : 8 },
+    { 'type' : 'laundry', 'room' : '326','time' : 15 },
+    { 'type' : 'dry', 'room': '326',  'time' : 14 },
+    { 'type' : 'laundry', 'room' : '325',  'time' : 16 },
+    { 'type' : 'laundry', 'room' : '326',  'time' : 16 },
+    { 'type' : 'dry', 'room' : '326',  'time' : 19 },
+    { 'type' : 'laundry', 'room' : '325',  'time' :19 },
+    { 'type' : 'dry', 'room' : '325',  'time': 21 }, ]
+
+   now = 13
+
+   return render_template('table.html', todayReservations=todayReservations,tomorrowReservations=tomorrowReservations, now=now)
 
 if __name__ == '__main__':  
    app.run('0.0.0.0',port=5000,debug=True)
