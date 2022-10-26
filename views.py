a132afeea6530e868746beb70e720472f05ffacb
김준철 <kimjoon889@gamil.com>
2022-10-26 10:26:24 +0900

    11

--- views.py
+++ views.py
@@ -1,12 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, make_response, render_template, request
 app = Flask(__name__)
 
 
 ## HTML을 주는 부분
 @app.route('/')
 def home():
-
-
 
    todayReservations = [
       { 'type' : 'laundry', 'room' : '325', 'time' : 7 },
@@ -36,6 +34,19 @@ def home():
    now = 13
 
    return render_template('table.html', todayReservations=todayReservations,tomorrowReservations=tomorrowReservations, now=now)
+
+@app.route('/memo')
+def memo():
+   info = "1212##12#"
+   resp = make_response()
+   resp.set_cookie('myinfo',info)
+   return resp
+
+@app.route('/get_cookie')
+def get_cookie():
+   print(request.cookies.get('myinfo'))
+   return request.cookies.get('myinfo')
+   
 
 if __name__ == '__main__':  
    app.run('0.0.0.0',port=5000,debug=True)
