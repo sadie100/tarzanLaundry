0f7a86f12151a8727f0c2eb1b062976703c75137
김준철 <kimjoon889@gamil.com>
2022-10-26 22:34:55 +0900

    10/22_준철로그인

--- static/index.js
+++ static/index.js
@@ -3,3 +3,13 @@
 const  chechk = function (a,b) {
     console.log(a,b)
 }
+
+const logOut = function() {
+    fetch('/logout',
+  {
+    method: 'GET',
+  }).then(location=location)
+  .then(location=location)
+  .then(location=location)
+}
+
