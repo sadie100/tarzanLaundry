0d8a48ca3a610abdcec92d38c317e7372edf9e68
김세희 <sadie1001@naver.com>
2022-10-26 20:24:17 +0900

    feat : 테이블 클릭 css 구현

--- static/index.js
+++ static/index.js
@@ -9,6 +9,14 @@ const checkMyCookie = function() {
     .then((response) => response.json())
 }
 
-const  chechk = function (a,b) {
-    console.log(a,b)
+// 시간 블럭 선택했을 때 함수
+const pick = function (time, day) {
+    //picked를 갖고 있는 엘리먼트의 전체 클래스 반환
+    const beforeClass = document.getElementsByClassName('picked');
+
+    if(beforeClass.length>0){
+        const slicedClass = beforeClass.item(0).className.slice(0,-6);
+        beforeClass.item(0).className = slicedClass
+    }
+    document.getElementById(`${day}-${time}`).className+=" picked";
 }
